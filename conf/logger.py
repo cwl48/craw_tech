510d7bcd346db18db7a4d210cd83278653b5645e
liwc@shinemo.com <199548lwc>
2018-05-19 15:40:38 +0800

    日志位置修改

--- conf/logger.py
+++ conf/logger.py
@@ -5,7 +5,7 @@ import os
 
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log.conf')
 
-server_log_path = "/data/server/crawl/"
+server_log_path = "/data/server/logs/crawl/"
 
 
 def init():
