fad6e8c0db9c19237f832f75c5cdc2e8879f66ca
liwc@shinemo.com <199548lwc>
2018-05-19 17:33:16 +0800

    爬虫任务在linux上启动，其他平台直接执行

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import platform
+
 from conf import logger
 from base import scheduled
-from crawler import juejin
+from crawler import juejin, cnblog, kf_toutiao, tuiku, importnew
+from util import myos
+
+
+def start():
+    # linux下开起定时任务  其他就直接全部执行
+    if myos.is_linux():
+        scheduled.start()
+    else:
+        juejin.JueJin().start()
+        tuiku.TuiKu().start()
+        kf_toutiao.KaiFaTouTiao().start()
+        importnew.ImportNew().start()
+        cnblog.CnBlog().start()
+
 
 if __name__ == '__main__':
     # log配置
     logger.init()
-
-    # 爬取定时任务启动
-    scheduled.start()
+    # 启动
+    start()
